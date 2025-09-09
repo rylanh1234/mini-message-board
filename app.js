4b1361f034706373e10be338b7c906bfe0a6dfe2
Rylan Ho <170477157+rylanh1234@users.noreply.github.com>
2025-09-09 17:28:43 -0600

    Created sample messages and displays them by rendering an EJS template file (index.ejs)

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require("express");
 const app = express();
 const newRouter = require("./routes/newRouter");
 const indexRouter = require("./routes/indexRouter");
+const path = require("node:path");
 
 app.use("/new", newRouter);
 app.use("/", indexRouter);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 const PORT = 3000;
 app.listen(PORT, (error) => {
