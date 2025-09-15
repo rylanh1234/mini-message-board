a87c202bd4e1fae8af744b633cf7f5ff32377b93
Rylan Ho <170477157+rylanh1234@users.noreply.github.com>
2025-09-15 16:22:16 -0600

    Added link for more message details by using route params

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,16 +2,19 @@ const { Router } = require("express");
 
 const indexRouter = Router();
 
+let id = 0;
 const messages = [
     {
         text: "Hi there!",
         user: "Amando",
-        added: new Date()
+        added: new Date(),
+        id: id
     },
     {
         text: "Hello World!",
         user: "Charles",
-        added: new Date()
+        added: new Date(),
+        id: id += 1
     },
 ];
 
@@ -20,8 +23,13 @@ indexRouter.get("/", (req, res) => {
 });
 indexRouter.post("/new", (req, res) => {
     const formData = req.body;
-    messages.push({ text: formData.messageText, user: formData.authorName, added: new Date() });
+    messages.push({ text: formData.messageText, user: formData.authorName, added: new Date(), id: id +=1 });
     res.redirect("/");
 }); // accesses form data, pushes the message, then redirects user back to index page
+
+indexRouter.get("/:messageID", (req, res) => {
+    const { messageID } = req.params;
+    res.send(`Message ID: ${messageID}`);
+});
 
 module.exports = indexRouter;
